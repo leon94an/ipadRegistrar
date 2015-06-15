cd36d4c9b530cf010ee590b3c7fb3ef6b67e7862
leon94an <la2534@columbia.edu>
2015-06-15 12:18:17 +0800

    added delete functionality

--- client/templates/ipads/ipads_list.js
+++ client/templates/ipads/ipads_list.js
@@ -1,5 +1,15 @@
 Template.ipadsList.helpers({
-  ipads: function() {
-    return Ipads.find();
-  }
+    ipads: function() {
+        return Ipads.find();
+    }
 });
+
+Template.ipadsList.events({
+    'click .delete': function(e) {
+        e.preventDefault();
+        if (confirm("Delete this ipad?")) {
+            var currentPostId = this._id;
+            Ipads.remove(currentPostId);
+        }
+    }
+})
